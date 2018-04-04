ffbf05254646199b9b742499add4b3e890a26c74
Tom Seagrave <tomseagrave94@gmail.com>
2018-04-04 10:59:06 +0100

    Determine what columns to use for training

--- multiPogDemo.py
+++ multiPogDemo.py
@@ -7,8 +7,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 
-# Import formated data
-df_products= pd.read_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/allProducts.csv', index_col='UPC')
+# Currently we import a formated product list that only includes columns for the classifier to consider
+# We need to change this so that the program can determine which columns to use
+df_products_complete = pd.read_csv(
+    '/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/completeProductData.csv', index_col='UPC')
+
+# Get the number of products from the dataframe, need this to compare against number of distinct values
+productCount = df_products_complete.shape[0]
+
+# The aim is to convert df_products_complete so that is matches df_products, for this we need to scan all the columns and count unique values
+for column in df_products_complete:
+    columnCount = df_products_complete[column].unique().size
+    print("The cound of unique values in column ", column, " is ", columnCount)
+    if not (1 < columnCount < productCount*0.5):
+        print("Dropping column ", column)
+        df_products_complete.drop(column, 1, inplace=True)
+
+for column in df_products_complete:
+    print(column)
+
+ # Import formated data
+#df_products = pd.read_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/allProducts.csv', index_col='UPC')
+df_products = df_products_complete
 # df = pd.read_csv('F:\Google Drive\Documents\Code\multiPogAI\data\data.csv', index_col='UPC')
 
 # Create dictionary to store encoding
@@ -18,7 +38,8 @@ d = defaultdict(LabelEncoder)
 df_products_encoded = df_products.apply(lambda x: d[x.name].fit_transform(x))
 
 # Import the training data, products on the current planograms
-df_train = pd.read_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/trainingData.csv', index_col='UPC')
+df_train = pd.read_csv(
+    '/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/trainingData.csv', index_col='UPC')
 # Use the dictionary to convert string values into ints
 df_train_encoded = df_train.apply(lambda x: d[x.name].transform(x))
 
@@ -34,35 +55,40 @@ testClassifier = tree.DecisionTreeClassifier()
 testClassifier.fit(x_train, y_train)
 testPredictions = testClassifier.predict(x_test)
 
-print("Accuracy from testing",accuracy_score(y_test, testPredictions),"%")
+print("Accuracy from testing", accuracy_score(y_test, testPredictions), "%")
 
 # Create new classifier using all the data to train it
 classifier = tree.DecisionTreeClassifier()
-classifier.fit(x,y)
+classifier.fit(x, y)
 
 # Load products without a planograms, and convert text using dictionary
-df_test = pd.read_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/testingData.csv', index_col='UPC')
+df_test = pd.read_csv(
+    '/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/testingData.csv', index_col='UPC')
 df_test_fit = df_test.apply(lambda x: d[x.name].transform(x))
 
 # Predict returns a numpy array, convert to data frame using index from test data
-df_predictions_encoded = pd.DataFrame(classifier.predict(df_test_fit), columns=['Planogram'], index=df_test_fit.index)
+df_predictions_encoded = pd.DataFrame(classifier.predict(
+    df_test_fit), columns=['Planogram'], index=df_test_fit.index)
 # Use the dictionary to revert the transform from int back to string
-df_predictions = df_predictions_encoded.apply(lambda x: d[x.name].inverse_transform(x))
+df_predictions = df_predictions_encoded.apply(
+    lambda x: d[x.name].inverse_transform(x))
 
 # Export results to CSV
-df_predictions.to_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/results.csv', sep=',')
+df_predictions.to_csv(
+    '/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/results.csv', sep=',')
 
 # Check the UPCs in results that we have planogram information for
-df_predictions_test = pd.merge(df_train['Planogram'].to_frame().rename(columns = {'Planogram':'SourcePlanogram'}), df_predictions, left_index=True, right_index=True)
+df_predictions_test = pd.merge(df_train['Planogram'].to_frame().rename(columns={
+    'Planogram': 'SourcePlanogram'}), df_predictions, left_index=True, right_index=True)
 
 #df_predictions_test.to_csv('/Users/thomasseagrave/Google Drive/Documents/Code/multiPogAI/data/resultsTest.csv', sep=',')
 
-#UPCs in the source and results now stores in df_predictions_test loop over and work out % correct
+# UPCs in the source and results now stores in df_predictions_test loop over and work out % correct
 totalCount = 0
 correctCount = 0
 for index, row in df_predictions_test.iterrows():
     totalCount += 1
     if(row['SourcePlanogram'] == row['Planogram']):
-        correctCount +=1
-    
+        correctCount += 1
+
 print("Accuracy from Source to Results is ", correctCount/totalCount*100)
